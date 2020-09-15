7c4b407565570f9cdd4d113c8c160ad3f8fbc0cf
phatnt <phatnt@sendo.vn>
2020-09-15 10:50:40 +0700

    Show articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
-function Articles() {
-
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+function Articles({ articles }) {
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.map(({ title, date, upvotes }) => (
+            <tr key={`${title}-${date}`} data-testid="article">
+              <td data-testid="article-title">{title}</td>
+              <td data-testid="article-upvotes">{upvotes}</td>
+              <td data-testid="article-date">{date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
